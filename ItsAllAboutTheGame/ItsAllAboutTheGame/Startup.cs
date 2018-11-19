af1560c1ad1b5ff3a7ac0b72d86ccbfd22058e62
Edwardchorius <edwardcho96@abv.bg>
2018-11-19 11:36:21 +0200

    Set the skeleton for Login Provider Implementation

--- ItsAllAboutTheGame/ItsAllAboutTheGame/Startup.cs
+++ ItsAllAboutTheGame/ItsAllAboutTheGame/Startup.cs
@@ -11,14 +11,19 @@ using Microsoft.Extensions.DependencyInjection;
 using ItsAllAboutTheGame.Data;
 using ItsAllAboutTheGame.Models;
 using ItsAllAboutTheGame.Services;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 namespace ItsAllAboutTheGame
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -26,17 +31,30 @@ namespace ItsAllAboutTheGame
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<ItsAllAboutTheGameDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddEntityFrameworkStores<ItsAllAboutTheGameDbContext>()
                 .AddDefaultTokenProviders();
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            /* 
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddOpenIdConnect(options =>
+            {
+                _configuration.Bind({ "providerKey"}, options);
+            })
+            .AddCookie();
+        */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,7 +71,12 @@ namespace ItsAllAboutTheGame
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseRewriter(new RewriteOptions()
+                .AddRedirectToHttpsPermanent()); // added SSL / for OIDC
+
             app.UseStaticFiles();
+
+            app.UseAuthentication(); // for OIDC
 
             app.UseAuthentication();
 
