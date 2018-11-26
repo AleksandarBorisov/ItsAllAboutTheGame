bba53d49177ea3ac7d761ca843d976dc8d8fa44b
AleksandarBorisov <a_borisov@abv.bg>
2018-11-26 13:13:39 +0200

    Fixed View Component not creating Wallet object

--- ItsAllAboutTheGame/ItsAllAboutTheGame/ViewComponents/LoginStatusViewComponent.cs
+++ ItsAllAboutTheGame/ItsAllAboutTheGame/ViewComponents/LoginStatusViewComponent.cs
@@ -3,6 +3,8 @@ using ItsAllAboutTheGame.Models.AccountViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,6 +19,23 @@ namespace ItsAllAboutTheGame.ViewComponents
         {
             this.singInManager = singInManager;
             this.userManager = userManager;
+            map = CultureInfo
+                .GetCultures(CultureTypes.AllCultures)
+                .Where(c => !c.IsNeutralCulture)
+                .Select(culture =>
+                {
+                    try
+                    {
+                        return new RegionInfo(culture.LCID);
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                })
+                .Where(ri => ri != null)
+                .GroupBy(ri => ri.ISOCurrencySymbol)
+                .ToDictionary(x => x.Key, x => x.First().CurrencySymbol);
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
@@ -27,13 +46,25 @@ namespace ItsAllAboutTheGame.ViewComponents
                 var user = await userManager.Users.Where(x => x.Id.Equals(userId))
                     .Include(player => player.Wallet)
                     .FirstOrDefaultAsync();
-                var info = new UserInfoViewModel(user);
+                var info = new UserInfoViewModel(user, TryGetCurrencySymbol(user.Wallet.Currency.ToString(), out string currSymbol) ? currSymbol : "");
                 return View("LoggedIn", info);
             }
             else
             {
                 return View();
             }
+        }
+
+        private static IDictionary<string, string> map;
+
+        //static CurrencyTools()
+        //{
+            
+        //}
+
+        public static bool TryGetCurrencySymbol(string ISOCurrencySymbol, out string symbol)
+        {
+            return map.TryGetValue(ISOCurrencySymbol, out symbol);
         }
     }
 }
