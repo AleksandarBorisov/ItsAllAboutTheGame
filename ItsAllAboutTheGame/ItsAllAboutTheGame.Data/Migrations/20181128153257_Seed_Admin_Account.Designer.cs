6d739fcf39da9267ade048586e6bd466c2012489
AleksandarBorisov <a_borisov@abv.bg>
2018-11-28 18:54:31 +0200

    Fixed login form position

--- ItsAllAboutTheGame/ItsAllAboutTheGame.Data/Migrations/20181128153257_Seed_Admin_Account.Designer.cs
+++ ItsAllAboutTheGame/ItsAllAboutTheGame.Data/Migrations/20181128153257_Seed_Admin_Account.Designer.cs
@@ -4,14 +4,16 @@ using ItsAllAboutTheGame.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ItsAllAboutTheGame.Data.Migrations
 {
     [DbContext(typeof(ItsAllAboutTheGameDbContext))]
-    partial class ItsAllAboutTheGameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181128153257_Seed_Admin_Account")]
+    partial class Seed_Admin_Account
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
