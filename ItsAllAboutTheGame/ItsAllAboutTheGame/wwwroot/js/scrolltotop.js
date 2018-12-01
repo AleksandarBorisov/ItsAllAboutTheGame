d6b473c0a360d65f7b664f6dfebf937b03609ae9
AleksandarBorisov <a_borisov@abv.bg>
2018-12-01 02:23:11 +0200

    Implemented Play Button and Access Denied Page

---
+++ ItsAllAboutTheGame/ItsAllAboutTheGame/wwwroot/js/scrolltotop.js
@@ -0,0 +1,18 @@
+﻿jQuery(document).ready(function () {
+
+    var btn = $('#scrollToTopBtn');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, '300');
+    });
+
+});
