4ff6d7b209e0eb6e0296227df58a7f1496056d09
Edwardchorius <edwardcho96@abv.bg>
2018-12-15 12:15:50 +0200

    Removed walletService tests due to refactoring in service

--- ItsAllAboutTheGame/ItsAllAboutTheGame.UnitTests/ServiceTests/CardServiceTests/DeleteCard_Should.cs
+++ ItsAllAboutTheGame/ItsAllAboutTheGame.UnitTests/ServiceTests/CardServiceTests/DeleteCard_Should.cs
@@ -1,79 +1,85 @@
-﻿//using ItsAllAboutTheGame.Data;
-//using ItsAllAboutTheGame.Data.Models;
-//using ItsAllAboutTheGame.GlobalUtilities.Enums;
-//using ItsAllAboutTheGame.Services.Data.Exceptions;
-//using ItsAllAboutTheGame.Services.Data.Services;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using ItsAllAboutTheGame.Data;
+using ItsAllAboutTheGame.Data.Models;
+using ItsAllAboutTheGame.GlobalUtilities;
+using ItsAllAboutTheGame.GlobalUtilities.Contracts;
+using ItsAllAboutTheGame.GlobalUtilities.Enums;
+using ItsAllAboutTheGame.Services.Data.Exceptions;
+using ItsAllAboutTheGame.Services.Data.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace ItsAllAboutTheGame.UnitTests.ServiceTests.CardServiceTests
-//{
-//    [TestClass]
-//    public class DeleteCard_Should
-//    {
-//        private DbContextOptions<ItsAllAboutTheGameDbContext> contextOptions;
-//        private CreditCard creditCard;
-//        private User user = new User();
+namespace ItsAllAboutTheGame.UnitTests.ServiceTests.CardServiceTests
+{
+    [TestClass]
+    public class DeleteCard_Should
+    {
+        private DbContextOptions<ItsAllAboutTheGameDbContext> contextOptions;
+        private CreditCard creditCard;
+        private User user = new User();
+        private IDateTimeProvider dateTimeProvider;
 
-//        [TestMethod]
-//        public async Task ThrowEntityNotFoundException_When_NoCardIsFound()
-//        {
-//            //Arrange
-//            contextOptions = new DbContextOptionsBuilder<ItsAllAboutTheGameDbContext>()
-//            .UseInMemoryDatabase(databaseName: "ThrowEntityNotFoundException_When_NoCardIsFound")
-//                .Options;
+        [TestMethod]
+        public async Task ThrowEntityNotFoundException_When_NoCardIsFound()
+        {
+            //Arrange
+            contextOptions = new DbContextOptionsBuilder<ItsAllAboutTheGameDbContext>()
+            .UseInMemoryDatabase(databaseName: "ThrowEntityNotFoundException_When_NoCardIsFound")
+                .Options;
 
-//            //Act & Assert
+            dateTimeProvider = new DateTimeProvider();
 
-//            using (var actContext = new ItsAllAboutTheGameDbContext(contextOptions))
-//            {
-//                var cardService = new CardService(actContext);
-//                int noCard = 0;
+            //Act & Assert
 
-//                await Assert.ThrowsExceptionAsync<EntityNotFoundException>(async () => await cardService.DeleteCard(noCard));
-//            }
-//        }
-        
-//        [TestMethod]
-//        public async Task ReturnCardToDelete_WhenValidParams_Passed()
-//        {
-//            //Arrange
-//            contextOptions = new DbContextOptionsBuilder<ItsAllAboutTheGameDbContext>()
-//            .UseInMemoryDatabase(databaseName: "ReturnCardToDelete_WhenValidParams_Passed")
-//                .Options;
+            using (var actContext = new ItsAllAboutTheGameDbContext(contextOptions))
+            {
+                var cardService = new CardService(actContext, dateTimeProvider);
+                int noCard = 0;
 
+                await Assert.ThrowsExceptionAsync<EntityNotFoundException>(async () => await cardService.DeleteCard(noCard));
+            }
+        }
 
-//            creditCard = new CreditCard
-//            {
-//                CardNumber = "23232141412",
-//                CVV = "3232",
-//                ExpiryDate = DateTime.Parse("02.03.2018"),
-//                User = user,
-//                UserId = user.Id,
-//            };
+        [TestMethod]
+        public async Task ReturnCardToDelete_WhenValidParams_Passed()
+        {
+            //Arrange
+            contextOptions = new DbContextOptionsBuilder<ItsAllAboutTheGameDbContext>()
+            .UseInMemoryDatabase(databaseName: "ReturnCardToDelete_WhenValidParams_Passed")
+                .Options;
 
 
-//            //Act 
-//            using (var actContext = new ItsAllAboutTheGameDbContext(contextOptions))
-//            {
-//                await actContext.CreditCards.AddAsync(creditCard);
-//                await actContext.SaveChangesAsync();
-//            }
+            creditCard = new CreditCard
+            {
+                CardNumber = "23232141412",
+                CVV = "3232",
+                ExpiryDate = DateTime.Parse("02.03.2018"),
+                User = user,
+                UserId = user.Id,
+            };
+
+            dateTimeProvider = new DateTimeProvider();
+
+            //Act 
+            using (var actContext = new ItsAllAboutTheGameDbContext(contextOptions))
+            {
+                await actContext.CreditCards.AddAsync(creditCard);
+                await actContext.SaveChangesAsync();
+            }
 
 
-//            //Assert
-//            using (var assertContext = new ItsAllAboutTheGameDbContext(contextOptions))
-//            {
-//                var cardService = new CardService(assertContext);
-//                var cardToDelete = await cardService.DeleteCard(creditCard.Id);
+            //Assert
+            using (var assertContext = new ItsAllAboutTheGameDbContext(contextOptions))
+            {
+                var cardService = new CardService(assertContext, dateTimeProvider);
+                var cardToDelete = await cardService.DeleteCard(creditCard.Id);
 
-//                Assert.IsInstanceOfType(cardToDelete, typeof(CreditCard));
-//                Assert.IsTrue(cardToDelete.IsDeleted);
-//            }
-//        }
-//    }
-//}
+                Assert.IsInstanceOfType(cardToDelete, typeof(CreditCard));
+                Assert.IsTrue(cardToDelete.IsDeleted);
+            }
+        }
+    }
+}
--- ItsAllAboutTheGame/ItsAllAboutTheGame.UnitTests/ServiceTests/CardServiceTests/DoesCardExist_Should.cs
+++ ItsAllAboutTheGame/ItsAllAboutTheGame.UnitTests/ServiceTests/CardServiceTests/DoesCardExist_Should.cs
@@ -1,120 +1,127 @@
-﻿//using ItsAllAboutTheGame.Data;
-//using ItsAllAboutTheGame.Data.Models;
-//using ItsAllAboutTheGame.GlobalUtilities.Enums;
-//using ItsAllAboutTheGame.Services.Data.Services;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System;
-//using System.Collections.Generic;
-//using System.Threading.Tasks;
+﻿using ItsAllAboutTheGame.Data;
+using ItsAllAboutTheGame.Data.Models;
+using ItsAllAboutTheGame.GlobalUtilities;
+using ItsAllAboutTheGame.GlobalUtilities.Contracts;
+using ItsAllAboutTheGame.GlobalUtilities.Enums;
+using ItsAllAboutTheGame.Services.Data.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-//namespace ItsAllAboutTheGame.UnitTests.ServiceTests.CardServiceTests
-//{   
-//    [TestClass]
-//    public class DoesCardExist_Should
-//    {
-//        private DbContextOptions<ItsAllAboutTheGameDbContext> contextOptions;
-//        private CreditCard creditCard;
-//        private User user;
+namespace ItsAllAboutTheGame.UnitTests.ServiceTests.CardServiceTests
+{
+    [TestClass]
+    public class DoesCardExist_Should
+    {
+        private DbContextOptions<ItsAllAboutTheGameDbContext> contextOptions;
+        private CreditCard creditCard;
+        private User user;
+        private IDateTimeProvider dateTimeProvider;
 
-//        [TestMethod]
-//        public async Task ReturnFalse_IfCardExists()
-//        {
-//            //Arrange
-//            contextOptions = new DbContextOptionsBuilder<ItsAllAboutTheGameDbContext>()
-//            .UseInMemoryDatabase(databaseName: "ReturnFalse_IfCardExists")
-//                .Options;
+        [TestMethod]
+        public async Task ReturnFalse_IfCardExists()
+        {
+            //Arrange
+            contextOptions = new DbContextOptionsBuilder<ItsAllAboutTheGameDbContext>()
+            .UseInMemoryDatabase(databaseName: "ReturnFalse_IfCardExists")
+                .Options;
 
-//            user = new User
-//            {
-//                Id = "randomId",
-//                Cards = new List<CreditCard>(),
-//                Transactions = new List<Transaction>(),
-//                UserName = "Koicho",
-//                CreatedOn = DateTime.Now,
-//                Email = "testmail@gmail",
-//                FirstName = "Koichokov",
-//                LastName = "Velichkov",
-//                DateOfBirth = DateTime.Parse("02.01.1996"),
-//                Role = UserRole.None,
-//            };
+            dateTimeProvider = new DateTimeProvider();
 
-//            creditCard = new CreditCard
-//            {
-//                CardNumber = "23232141412",
-//                CVV = "3232",
-//                ExpiryDate = DateTime.Parse("02.03.2020"),
-//                User = user,
-//                UserId = user.Id,
-//            };
+            user = new User
+            {
+                Id = "randomId",
+                Cards = new List<CreditCard>(),
+                Transactions = new List<Transaction>(),
+                UserName = "Koicho",
+                CreatedOn = dateTimeProvider.Now,
+                Email = "testmail@gmail",
+                FirstName = "Koichokov",
+                LastName = "Velichkov",
+                DateOfBirth = DateTime.Parse("02.01.1996"),
+                Role = UserRole.None,
+            };
 
-//            //Act 
-//            using (var actContext = new ItsAllAboutTheGameDbContext(contextOptions))
-//            {
-//                await actContext.Users.AddAsync(user);
-//                await actContext.CreditCards.AddAsync(creditCard);
-//                await actContext.SaveChangesAsync();
-//            }
+            creditCard = new CreditCard
+            {
+                CardNumber = "23232141412",
+                CVV = "3232",
+                ExpiryDate = DateTime.Parse("02.03.2020"),
+                User = user,
+                UserId = user.Id,
+            };
+
+            //Act 
+            using (var actContext = new ItsAllAboutTheGameDbContext(contextOptions))
+            {
+                await actContext.Users.AddAsync(user);
+                await actContext.CreditCards.AddAsync(creditCard);
+                await actContext.SaveChangesAsync();
+            }
 
 
-//            // Assert
-//            using (var assertContext = new ItsAllAboutTheGameDbContext(contextOptions))
-//            {
-//                var cardService = new CardService(assertContext);
-//                var doesCardExist = cardService.DoesCardExist(creditCard.CardNumber);
+            // Assert
+            using (var assertContext = new ItsAllAboutTheGameDbContext(contextOptions))
+            {
+                var cardService = new CardService(assertContext, dateTimeProvider);
+                var doesCardExist = cardService.DoesCardExist(creditCard.CardNumber);
 
-//                Assert.IsFalse(doesCardExist);
-//            }
-//        }
+                Assert.IsFalse(doesCardExist);
+            }
+        }
 
-//        [TestMethod]
-//        public async Task ReturnTrue_IfCardDoesntExist()
-//        {
-//            //Arrange
-//            contextOptions = new DbContextOptionsBuilder<ItsAllAboutTheGameDbContext>()
-//            .UseInMemoryDatabase(databaseName: "ReturnTrue_IfCardDoesntExist")
-//                .Options;
+        [TestMethod]
+        public async Task ReturnTrue_IfCardDoesntExist()
+        {
+            //Arrange
+            contextOptions = new DbContextOptionsBuilder<ItsAllAboutTheGameDbContext>()
+            .UseInMemoryDatabase(databaseName: "ReturnTrue_IfCardDoesntExist")
+                .Options;
 
-//            user = new User
-//            {
-//                Id = "randomId",
-//                Cards = new List<CreditCard>(),
-//                Transactions = new List<Transaction>(),
-//                UserName = "Koicho",
-//                CreatedOn = DateTime.Now,
-//                Email = "testmail@gmail",
-//                FirstName = "Koichokov",
-//                LastName = "Velichkov",
-//                DateOfBirth = DateTime.Parse("02.01.1996"),
-//                Role = UserRole.None,
-//            };
+            dateTimeProvider = new DateTimeProvider();
 
-//            creditCard = new CreditCard
-//            {
-//                CardNumber = "23232141412",
-//                CVV = "3232",
-//                ExpiryDate = DateTime.Parse("02.03.2020"),
-//                User = user,
-//                UserId = user.Id,
-//            };
+            user = new User
+            {
+                Id = "randomId",
+                Cards = new List<CreditCard>(),
+                Transactions = new List<Transaction>(),
+                UserName = "Koicho",
+                CreatedOn = dateTimeProvider.Now,
+                Email = "testmail@gmail",
+                FirstName = "Koichokov",
+                LastName = "Velichkov",
+                DateOfBirth = DateTime.Parse("02.01.1996"),
+                Role = UserRole.None,
+            };
 
-//            //Act 
-//            using (var actContext = new ItsAllAboutTheGameDbContext(contextOptions))
-//            {
-//                await actContext.Users.AddAsync(user);
-//                await actContext.CreditCards.AddAsync(creditCard);
-//                await actContext.SaveChangesAsync();
-//            }
+            creditCard = new CreditCard
+            {
+                CardNumber = "23232141412",
+                CVV = "3232",
+                ExpiryDate = DateTime.Parse("02.03.2020"),
+                User = user,
+                UserId = user.Id,
+            };
 
-//            // Assert
-//            using (var assertContext = new ItsAllAboutTheGameDbContext(contextOptions))
-//            {
-//                var cardService = new CardService(assertContext);
-//                //We pass in a different cardNumber which is not added  thus it does not exist in the context!
-//                var doesCardExist = cardService.DoesCardExist("23232112412");
+            //Act 
+            using (var actContext = new ItsAllAboutTheGameDbContext(contextOptions))
+            {
+                await actContext.Users.AddAsync(user);
+                await actContext.CreditCards.AddAsync(creditCard);
+                await actContext.SaveChangesAsync();
+            }
 
-//                Assert.IsTrue(doesCardExist);
-//            }
-//        }
-//    }
-//}
+            // Assert
+            using (var assertContext = new ItsAllAboutTheGameDbContext(contextOptions))
+            {
+                var cardService = new CardService(assertContext, dateTimeProvider);
+                //We pass in a different cardNumber which is not added  thus it does not exist in the context!
+                var doesCardExist = cardService.DoesCardExist("23232112412");
+
+                Assert.IsTrue(doesCardExist);
+            }
+        }
+    }
+}
